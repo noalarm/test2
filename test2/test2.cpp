1d4a0bc063e1844ed08cb8b6ab0441033e09066b
Giorgyy Mayorov <yayestyaunderstand@gmail.com>
2024-05-06 00:58:22 +0300

    Добавьте файлы проекта.

---
+++ test2/test2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string> 
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    string a, b;
+    getline(cin, a);
+    getline(cin, b);
+    try {
+        if (a.length() != b.length()) {
+            throw exception("Длины строк не равны");
+        }
+        cout << "Длины строк равны";
+    }
+    catch (exception e) {
+        cout << e.what() << endl;
+    }
+}
